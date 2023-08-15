c9f2691ad93d170a1ad458ae9c07819ce38ef92a
rodainaKhalil <rodaina.khalil@student.guc.edu.eg>
2023-08-15 15:31:16 +0300

    finish

--- Files/Script.js
+++ Files/Script.js
@@ -1,63 +1,97 @@
-// // Script.js
 
-// const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
-// const baseUrl = 'https://www.dictionaryapi.com/api/v3/references/learners/json/';
+// const searchBar = document.getElementById("input");
+// const wordDiv = document.getElementById("word");
+// const phoneticsDiv = document.getElementById("phonetics");
+// const definitionDiv = document.getElementById("definition");
+// const pronunciationButton = document.getElementById("pronunciation");
 
-// const searchBar = document.querySelector('.input');
-// const phoneticsDiv = document.querySelector('.phonetics');
-// const definitionDiv = document.querySelector('.definition');
-// const pronunciationButton = document.querySelector('.pronunciation');
-// const audioElement = new Audio();
+// async function fetchAPI(word) {
+//     try {
+//       infoTextEl.style.display = "block";
+//       meaningContainerEl.style.display = "none";
+//       infoTextEl.innerText = `Searching the meaning of "${word}"`;
+//       const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+//       const result = await fetch(url).then((res) => res.json());
+  
+//       if (result.title) {
+//         meaningContainerEl.style.display = "block";
+//         infoTextEl.style.display = "none";
+//         wordDiv.innerText = word;
+//         definitionDiv.innerText = "N/A";
+//         pronunciationButton.style.display = "none";
+//       } else {
+//         infoTextEl.style.display = "none";
+//         meaningContainerEl.style.display = "block";
+//         pronunciationButton.style.display = "inline-flex";
+//         wordDiv.innerText = result[0].word;
+//         definitionDiv.innerText = result[0].meanings[0].definitions[0].definition;
+//         pronunciationButton.src = result[0].phonetics[0].audio;
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       infoTextEl.innerText = `an error happened, try again later`;
+//     }
+//   }
+  
+//   searchBar.addEventListener("keyup", (e) => {
+//     if (e.target.value && e.key === "Enter") {
+//       fetchAPI(e.target.value);
+//     }
+//   });
 
-// searchBar.addEventListener('keypress', event => {
+
+// // searchBar.addEventListener('keypress', event => {
+// //     if (event.key === 'Enter') {
+// //         const word = searchBar.value;
+// //         wordDiv.textContent = `${word}`;
+// //         phoneticsDiv.textContent = `Pho - Ne - Tics`;
+// //         definitionDiv.textContent = `Definition of ${word}`;
+// //     }
+// // });
+
+const inputEl = document.getElementById("input");
+const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title");
+const meaningEl = document.getElementById("meaning");
+const audioEl = document.getElementById("audio");
+
+async function fetchAPI(word) {
+  try {
+    infoTextEl.style.display = "block";
+    meaningContainerEl.style.display = "none";
+    infoTextEl.innerText = `Searching the meaning of "${word}"`;
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const result = await fetch(url).then((res) => res.json());
+
+    if (result.title) {
+      meaningContainerEl.style.display = "block";
+      infoTextEl.style.display = "none";
+      titleEl.innerText = word;
+      meaningEl.innerText = "N/A";
+      audioEl.style.display = "none";
+    } else {
+      infoTextEl.style.display = "none";
+      meaningContainerEl.style.display = "block";
+      audioEl.style.display = "inline-flex";
+      titleEl.innerText = result[0].word;
+      meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+      audioEl.src = result[0].phonetics[0].audio;
+    }
+  } catch (error) {
+    console.log(error);
+    infoTextEl.innerText = `an error happened, try again later`;
+  }
+}
+
+inputEl.addEventListener("keyup", (e) => {
+  if (e.target.value && e.key === "Enter") {
+    fetchAPI(e.target.value);
+  }
+});
+
+// inputEl.addEventListener('keypress', event => {
 //     if (event.key === 'Enter') {
-//         event.preventDefault(); // Prevent form submission
-//         const word = searchBar.value;
-//         fetch(`${baseUrl}${word}?key=${apiKey}`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 if (Array.isArray(data) && data.length > 0) {
-//                     const firstEntry = data[0];
-//                     if (firstEntry.hwi && firstEntry.hwi.prs) {
-//                         const phoneticText = firstEntry.hwi.prs[0].mw;
-//                         phoneticsDiv.textContent = `Phonetics: ${phoneticText}`;
-//                     }
-
-//                     if (firstEntry.shortdef) {
-//                         const definitionText = firstEntry.shortdef[0];
-//                         definitionDiv.textContent = `Definition: ${definitionText}`;
-//                     }
-
-//                     if (firstEntry.hwi && firstEntry.hwi.prs[0].sound) {
-//                         const soundFile = firstEntry.hwi.prs[0].sound.audio;
-//                         pronunciationButton.addEventListener('click', () => {
-//                             audioElement.src = `https://media.merriam-webster.com/soundc11/${soundFile[0]}/${soundFile}.wav`;
-//                             audioElement.play();
-//                         });
-//                     }
-//                 } else {
-//                     console.log('No data found for the word:', word);
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching data:', error);
-//             });
+//         fetchAPI(event.target.value);
 //     }
 // });
-
-// // ... Other event listeners ...
-
-
-const searchBar = document.querySelector('.input');
-const wordDiv = document.querySelector('.word');
-const phoneticsDiv = document.querySelector('.phonetics');
-const definitionDiv = document.querySelector('.definition');
-
-searchBar.addEventListener('keypress', event => {
-    if (event.key === 'Enter') {
-        const word = searchBar.value;
-        wordDiv.textContent = `${word}`;
-        phoneticsDiv.textContent = `Pho - Ne - Tics`;
-        definitionDiv.textContent = `Definition of ${word}`;
-    }
-});
